c2451cbbd897c32dfa40f0bb5b2fd2ccc803be21
Tomvictor <vjtomvictor@icloud.com>
2016-09-12 10:06:22 +0530

    how to use external .js file, in html

---
+++ external.js
@@ -0,0 +1,8 @@
+function myFunction() {
+		// body...
+		document.getElementById('demo').innerHTML = "hai I am the changed text block ";
+	}
+function myFunction2() {
+		// body...
+		document.getElementById('demo').style.fontSize='35px';
+}
